7d2c4c66d36e2427ca2a3362ed4d4b8c4e878078
sanjeev kumar <sanjeev.kumar@mcwayweb.com>
2015-06-28 19:32:48 +0530

    url fixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from . import viewsback
 from .views import auth,verificationlinks,profile,passwords
 from django.core.urlresolvers import reverse_lazy
 urlpatterns = [
@@ -9,7 +8,7 @@ urlpatterns = [
         url(r'^dashboard$',profile.DashBoardView.as_view(),name='dashboard'),
         url(r'^verifyeaccount$',verificationlinks.ResendConfirmationLinkView.as_view(),name='reconfirm'),
         url(r'^profile(?:/(?P<file_upload>[a-zA-Z]+))?$',profile.ProfileView.as_view(),name='profile'),
-        url(r'^confirm/(?P<userslug>[-\w]+)/(?P<activationkey>[-\w]+)$',viewsback.EmailConfirmation.as_view(),name='emailconfirmation'),
+        url(r'^confirm/(?P<userslug>[-\w]+)/(?P<activationkey>[-\w]+)$',verificationlinks.EmailConfirmation.as_view(),name='emailconfirmation'),
         url(r'^password/post-passwordchange$',passwords.ChangePasswordView.as_view(),name='postpasswordchange'),
         url(r'^password/changepassword$', 'django.contrib.auth.views.password_change', {'post_change_redirect' : reverse_lazy('accounts:postpasswordchange'),'template_name': 'accounts/changepassword.html'},name='changepassword'),
         url(r'^profileimage',profile.ChangeprofilePic.as_view(),name='changeprofilepic'),
