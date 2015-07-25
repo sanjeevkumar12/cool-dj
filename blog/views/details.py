1e9239b506a14a575e14c801d153ed779c614282
sanjeev kumar <sanjeev.kumar@mcwayweb.com>
2015-07-25 20:36:32 +0530

    blog tags and blog detail template

--- blog/views/details.py
+++ blog/views/details.py
@@ -1,13 +1,17 @@
 from django.views.generic import DetailView,ListView
 from ..models import Post,Category
-class SlugDetailView(DetailView):
+from .mixin import BlogMixin
+class SlugDetailView(BlogMixin,DetailView):
     model = Post
+    template_name = 'blog/post-detail.html'
 
     def get_object(self, queryset=None):
         try:
-            return Post.objects.published().filter(slug=self.kwargs.get('slug'))
+            return Post.objects.published().filter(slug=self.kwargs.get('slug')).get()
         except Post.DoesNotExist:
             return None
+    def render_to_response(self, context, **response_kwargs):
+        return super(SlugDetailView,self).render_to_response( context, **response_kwargs)
 
 class CategoryDetailView(ListView):
     model = Post
